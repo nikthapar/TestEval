00f02cca351f6e62ca9875317a1eeb820e862d05
Anish <anishkap7010@gmail.com>
2018-02-08 08:22:55 +0530

    Keywords check completed

--- index.js
+++ index.js
@@ -1,16 +1,63 @@
-/*var fs = require('fs');
+//fs node js .. taking up only .txt files
+var fs = require('fs');
+//document to be evaluated
+var document = fs.readFileSync('document.txt','utf-8');
 
-fs.readFile('try.txt','utf-8',function(error,data) {
-	console.log(data);													//fs node js .. taking up only .txt files
-});
-*/
+//keyWords file
+var keyWords = fs.readFileSync('keyWords.txt','utf-8');
 
-var textract = require('textract');
 
-textract.fromFileWithPath('Assignment.docx', function( error, text ) {			//able to take wprd files
-	if(error){
-		console.log("Shut it");
+var natural = require('natural');
+
+var tokenizer = new natural.WordTokenizer();
+//for tokenizing
+var token_document = tokenizer.tokenize(document);
+console.log(token_document);
+var token_keyWords = tokenizer.tokenize(keyWords);
+
+//gives the number of words
+var length = token_document.length;
+console.log("Document contains "+length+"words.");
+
+
+if(length>=1000 && length<=4000)
+	console.log("Word Check Complete : It's valid")
+
+else {
+	console.log("Invalid number of words.");
+	//exit program here
+}
+
+var marks = 0;
+var correctWords = [];
+var spellcheck = new natural.Spellcheck(token_document);
+
+for(i in token_keyWords){
+	if(spellcheck.isCorrect(token_keyWords[i])){
+		marks++;
+		correctWords.push(token_keyWords[i]);
 	}
-	else
-		console.log(text);
-});
+}
+
+console.log("These are the concepts that you have covered : \n"+correctWords+"\n and you get these marks for that :"+marks);
+
+console.log("You get these many marks : "+marks);
+
+//dictionary and its tokenization
+var dictionary = fs.readFileSync('dictionary.txt','utf-8');
+var token_dictionary = tokenizer.tokenize(dictionary);
+console.log(token_dictionary);
+
+var spellcheck1 = new natural.Spellcheck(token_dictionary);
+var mistakes = 0;
+var incorrectWords = [];
+
+for(i in token_document){
+	if(!spellcheck1.isCorrect(token_document[i])){
+		mistakes++;
+		incorrectWords.push(token_document[i]);
+	}
+}
+
+console.log("These are the spelling mistakes you had : \n"+incorrectWords+"and you have these many mistakes \n: "+mistakes);
+
