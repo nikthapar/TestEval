acbd830e1f326f81a1af6c80fe8147592de9c68c
Anish <anishkap7010@gmail.com>
2018-02-09 15:46:58 +0530

    Spellcheck and word count working

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
-//fs node js .. taking up only .txt files
+//fs node js .. taking up only .txt files and natural
 var fs = require('fs');
+var natural = require('natural');
+var tokenizer = new natural.WordTokenizer();
+
 //document to be evaluated
 var document = fs.readFileSync('document.txt','utf-8');
-
 //keyWords file
 var keyWords = fs.readFileSync('keyWords.txt','utf-8');
-
-
-var natural = require('natural');
-
-var tokenizer = new natural.WordTokenizer();
 //for tokenizing
 var token_document = tokenizer.tokenize(document);
-console.log(token_document);
+//console.log(token_document);
 var token_keyWords = tokenizer.tokenize(keyWords);
 
 //gives the number of words
@@ -25,7 +22,7 @@ if(length>=1000 && length<=4000)
 
 else {
 	console.log("Invalid number of words.");
-	//exit program here
+	return;
 }
 
 var marks = 0;
@@ -39,25 +36,28 @@ for(i in token_keyWords){
 	}
 }
 
+ 
 console.log("These are the concepts that you have covered : \n"+correctWords+"\n and you get these marks for that :"+marks);
-
+console.log(correctWords);
 console.log("You get these many marks : "+marks);
+
 
 //dictionary and its tokenization
 var dictionary = fs.readFileSync('dictionary.txt','utf-8');
 var token_dictionary = tokenizer.tokenize(dictionary);
-console.log(token_dictionary);
 
+//checking spellings
 var spellcheck1 = new natural.Spellcheck(token_dictionary);
 var mistakes = 0;
 var incorrectWords = [];
 
 for(i in token_document){
-	if(!spellcheck1.isCorrect(token_document[i])){
+	if(!spellcheck1.isCorrect(token_document[i].toLowerCase())){
 		mistakes++;
 		incorrectWords.push(token_document[i]);
 	}
 }
 
 console.log("These are the spelling mistakes you had : \n"+incorrectWords+"and you have these many mistakes \n: "+mistakes);
+
 
