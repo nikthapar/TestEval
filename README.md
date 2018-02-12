f843aa46640f88403dcfe9283851ed83d5f4876f
nikhil <nikky101403122@gmail.com>
2018-02-12 15:44:50 +0530

    json

--- README.md
+++
@@ -1,9 +0,0 @@
-# DocTest
-
-~Version 1.0
-
-1. Upload your text documents which you want to compare with the standard document.
-2. Run the index.js program which generates a output.json file.
-3. Then make the json server.
-4. Run the index.html file.
-5. And you get the output.
--- index.js
+++ index.js
@@ -1,22 +1,19 @@
-//fs node js .. taking up only .txt files and natural
+
 var fs = require('fs');
 var natural = require('natural');
 var tokenizer = new natural.WordTokenizer();
 var WordPOS = require('wordpos'),
 wordpos = new WordPOS();
 
-//document to be evaluated
-var evalDocument = fs.readFileSync('./documents/EvalDocument.txt','utf-8');
-//standard document
-var standardDocument = fs.readFileSync('./documents/StandardDocument.txt','utf-8');
+var evalDocument = fs.readFileSync('eCss.txt','utf-8');
+var standardDocument = fs.readFileSync('sCss.txt','utf-8');
 
-//tokenizing to get the word count
-//length of standard document
+
 var standardTokens = tokenizer.tokenize(standardDocument);
 var evalTokens = tokenizer.tokenize(evalDocument);
-//twenty percent length relaxation
+
 var range = (standardTokens.length*20)/100
-//lengths of respective documents
+
 var standardLength = standardTokens.length;
 var evalLength = evalTokens.length;
 
@@ -25,11 +22,10 @@ if(evalLength <= standardLength-range || evalLength >= standardLength+range){
   return;
 }
 
-//global variables
 var evalNouns;
 var standardNouns;
 var similarNouns = [];
-//calculating nouns back to back and calling json file creation function in end
+
 wordpos.getNouns(evalDocument, function(result){
   evalNouns = result;
   
@@ -39,23 +35,22 @@ wordpos.getNouns(evalDocument, function(result){
     })
 });
 
-//comparing nouns of both documents and calculating noun percentage
+
 function compare(){
   var corpus = standardNouns;
   var spellcheck = new natural.Spellcheck(corpus);
-  //comparing each noun of evalDoc with corpus
+
   for(let i = 0; i < evalNouns.length; i++){
 	if(spellcheck.isCorrect(evalNouns[i])){
 		similarNouns.push(evalNouns[i]);
 	}
 }   
-    console.log(similarNouns)
-    end();
+    nik();
 } 
 
-//json file -- to be executed in the end
- function end(){
-    var score = {
+
+ function nik(){
+    var result = {
       Standard : 
         {
           wordCount : standardLength,
@@ -69,8 +64,8 @@ function compare(){
           commonNouns : similarNouns.length
         }
     }
-      //object to string
-  var json = JSON.stringify(score, null, 2);
-  //storing it in .json file
-  fs.writeFileSync('output.json',json)
+
+  var json = JSON.stringify(result, null, 2);
+
+  fs.writeFileSync('data.json',json)
  }
